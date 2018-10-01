ddbe09aa9633aa75a6e7919a3cb93d4d9d029fdb
Kelly Sheddy <jungaka07@gmail.com>
2018-10-01 19:33:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,20 @@
-katz_deli = []
+katz_deli = [] #there is nobody in line to start
 
-def line(katz_deli)
-  if katz_deli.length == 0
+def line(katz_deli) 
+  if katz_deli.length == 0 #if the line equals zero, say phrase
     puts "The line is currently empty."
   else
-   customers = "The line is currently:"
-    katz_deli.each_with_index do |customer, index|
-    customers +=  " #{index + 1}. #{customer}"
+   customers = "The line is currently:" #if there are customers
+    katz_deli.each_with_index do |customer, index|  
+      
+      #in the array, take the values AND index numbers
+    
+    customers +=  " #{index + 1}. #{customer}" 
+    
+    #after you gather those values/index numbers, add one to the index amount and maintain the customer value
     end
   puts customers
+  #prints current customer number in line
 end
 end
 
